6da546239cf8efd8c311738eab708c3847ffb85c
Harmeet <phpexpert00784@gmail.com>
2025-11-23 08:54:56 +1300

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+
+# This is example of string
+name = "John Doe"
+print(name)
+
+# This is example of Integer
+age = "20"
+print(age)
+
+# This is example of lists
+skills = ["Python", "SQL", "Power BI"]
+print(skills)
+
+# This is example of tuple
+education = ("BSc Computer Science", 2020)
+print(education)
+
+# This is example of dictionary
+contact =	{
+  "email": "harmeetsinghnagi.nz@gmail.com",
+  "contact": "+64226552480"
+}
+print(contact)
+
+# This is example of set
+certifications = {"Azure", "AWS", "Azure"}
+print(certifications)
